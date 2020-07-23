82941f1b03e1e4543120eb46b4f5c15e25e82ab6
Lys <wangshiwen002@deppon.com>
2020-07-23 13:30:15 +0800

    测试

---
+++ src/main/java/com/lys/thread/threadTest/ThreadTest.java
@@ -0,0 +1,15 @@
+package com.lys.thread.threadTest;
+
+/**
+ * @ClassName thisName
+ * @Description 实现多线程
+ * @Author Lys
+ * @Date 2020/7/22 13:24
+ * @Version 1.0
+ **/
+public class ThreadTest implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
